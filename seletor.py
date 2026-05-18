255aef4811bc77e1f9585a98d8fc3aab906fddfb
ravya629 <00001141332346sp@al.educacao.sp.gov.br>
2026-05-18 13:15:53 +0000

    v2

---
+++ seletor.py
@@ -0,0 +1,9 @@
+print("#--seletor de caracteres--🔠")
+print("escolha uma letra e veja se ela esta disponivel👀")
+letra = input("digite a letra: ").upper()
+if letra == "A":
+    print("voce escolheu a letra A 👌")
+elif letra == "B":
+    print ("voce escolheu a letra B 👌")
+else:
+    print ("essa opcao nao esta disponivel 😒")
