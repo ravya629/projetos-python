ffa99fa808fd679a0b747904bf67ed716814a48d
ravya629 <00001141332346sp@al.educacao.sp.gov.br>
2026-05-25 12:32:29 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamado a Função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvidoo no Codespace')
+    print('=' * 30)
+
+### Chamado  função
+exibir_cabecalho()
+print('Menu principal:')
