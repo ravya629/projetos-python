0ef706a85e483910c7bddbbc09bd4a59f5f8e0e7
ravya629 <00001141332346sp@al.educacao.sp.gov.br>
2026-06-22 12:53:58 +0000

    v2

--- playlist.py
+++ playlist.py
@@ -1,6 +1,7 @@
 print("--- Music player ---")
 playlist = ["Musica 01 - Rock", "Muscica 02 - Pop", "musica 03 - Rap"]
-atual = 0
-print(f"Tocando agora: {playlist[atual]}")
+atual = int(input("Digite o número da música"))
+indice = atual -1
+print(f"Tocando agora: {playlist[indice]}")
 print("Musica anterior: --(Inicio da playlist)")
 print("=" * 40)
