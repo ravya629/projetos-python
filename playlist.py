865d6c34499f8f4a94b31d001192297054bc69db
ravya629 <00001141332346sp@al.educacao.sp.gov.br>
2026-06-22 12:40:22 +0000

    v2

---
+++ playlist.py
@@ -0,0 +1,6 @@
+print("--- Music player ---")
+playlist = ["Musica 01 - Rock", "Muscica 02 - Pop", "musica 03 - Rap"]
+atual = 0
+print(f"Tocando agora: {playlist[atual]}")
+print("Musica anterior: --(Inicio da playlist)")
+print("=" * 40)
