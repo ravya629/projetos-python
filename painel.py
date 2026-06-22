0ac2efcfc938021346d603306d359542b8d8a02b
ravya629 <00001141332346sp@al.educacao.sp.gov.br>
2026-06-22 12:29:01 +0000

    v2

---
+++ painel.py
@@ -0,0 +1,8 @@
+print("--- PAINEL DE ATEDIMENTO ---")
+
+fila_espera = ["Senha 01", "Senha 02", "Senha 03", "Senha 04"]
+print(f"O proximo paciente a ser atendido é: {fila_espera[0]}")
+print(f"O segundo paciente da fila é: {fila_espera[1]}")
+
+total = len(fila_espera)
+print(f"total aguardando: {total}")
